87defb15c82553b6609ed62ddcef16318fe5fd8d
ig-tishin <igor.tishin.98@mail.ru>
2026-01-27 00:14:42 +0300

    HW1 commit

---
+++ Lesson 1 (01-docker-terraform)/HW/Q3-6/question4.sql
@@ -0,0 +1,15 @@
+
+SELECT 
+	lpep_pickup_datetime::DATE,
+	trip_distance
+FROM
+	green_tripdata_2025_11
+WHERE
+	trip_distance <= 100.0
+ORDER BY
+	trip_distance DESC
+LIMIT 
+	5;
+
+
+--answer 2025-11-14
